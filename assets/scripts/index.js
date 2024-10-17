d45efdcfcae2e65c3bd35feb374f0d4ee92321ac
Vitel <vitelrfs@outlook.com>
2024-10-17 09:48:22 -0300

    alterar tela de login

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,23 @@
 import { login, checkAuthStatusLogin, createAccount } from '../firebase/firebase_function_fe.js';
 import { mostrarNotificacao } from './alerts.js';
 
+const container = document.getElementById('container');
+const signUpButton = document.querySelector('.overlay-right button');
+const signInButton = document.querySelector('.overlay-left button');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+});
+
 
 document.getElementById('login-form').addEventListener('submit', async (event) => {
     event.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    const email = document.getElementById('email-login').value;
+    const password = document.getElementById('password-login').value;
 
     const isAuthenticated = await login(email, password);
     
@@ -16,16 +28,16 @@ document.getElementById('login-form').addEventListener('submit', async (event) =
 
 document.getElementById('register-form').addEventListener('submit', async (event) => {
     event.preventDefault();
-    const email = document.getElementById('email_register').value;
-    const password = document.getElementById('password_register').value;
-    const passwordRepeat = document.getElementById('password-repeat').value;
+    const email = document.getElementById('email-create').value;
+    const password = document.getElementById('password-create').value;
+    const passwordRepeat = document.getElementById('confirm-password-create').value;
 
     if (password !== passwordRepeat) {
         mostrarNotificacao('error', 'As senhas devem ser iguais', 'Erro no Cadastro');
         return;
     }
 
-    const name = document.getElementById('name').value;
+    const name = document.getElementById('name-create').value;
     const accountCreated = createAccount(email, password, name);
 
     if (accountCreated) {
