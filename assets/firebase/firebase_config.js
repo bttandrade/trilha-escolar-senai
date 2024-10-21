aafcffe86d8b8cc80bd6122fa9eedbd357c7b30f
Victor Reis Freitas <130703266+Vitelfs@users.noreply.github.com>
2024-10-21 13:06:26 -0300

    Update firebase_config.js

--- assets/firebase/firebase_config.js
+++ assets/firebase/firebase_config.js
@@ -1,12 +1,15 @@
 import admin from 'firebase-admin';
+import { createRequire } from 'module';
 
+const require = createRequire(import.meta.url);
 const serviceAccount = require('./trilha-educacional-5e1cb-firebase-adminsdk-pkl5h-0828d8aa0f.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'trilha-educacional-5e1cb.appspot.com'
 });
-
+const bucket = admin.storage().bucket();
 const auth = admin.auth();
 const db = admin.firestore();
 
-export { db, auth };
+export { db, auth, bucket };
